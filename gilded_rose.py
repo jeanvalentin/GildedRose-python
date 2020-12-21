055e106fd4e63a9f4f61bb019048a4cc91cd400c
Jean Valentin <jeanvalentin@users.noreply.github.com>
2020-12-21 19:00:38 +0100

    Refactor updateQuality in Item

--- gilded_rose.py
+++ gilded_rose.py
@@ -48,3 +48,43 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+    def update_quality(self):
+        self.compute_quality()
+        self.fix_quality()
+        self.compute_sell_in()
+
+    def compute_quality(self):
+        if Item.AGED == self.name:
+            self.quality += 1
+            if self.sell_in <= 0:
+                self.quality += 1
+            return
+        if Item.BACKSTAGE_PASSES == self.name:
+            self.quality += 1
+            if self.sell_in <= 10:
+                self.quality += 1
+            if self.sell_in <= 5:
+                self.quality += 1
+            if self.sell_in <= 0:
+                self.quality = 0
+            return
+        if Item.LEGENDARY == self.name:
+            return
+
+        self.quality -= 1
+        if self.sell_in <= 0:
+            self.quality -= 1
+
+    def fix_quality(self):
+        if Item.LEGENDARY == self.name:
+            return
+        if self.quality >= 50:
+            self.quality = 50
+        if self.quality <= 0:
+            self.quality = 0
+
+    def compute_sell_in(self):
+        if Item.LEGENDARY == self.name:
+            return
+        self.sell_in -= 1
